4298a3d270979d9ea781cbd36ff601b2ed5a65dd
Ajazkhan10 <99741323+Ajazkhan10@users.noreply.github.com>
2022-09-15 20:23:03 +0500

    simple popup

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <main>
+			<button @click="tttttt">click</button>
+      <h1>PopUp</h1>
+      <button @click="openPopUp">Open PopUp</button>
+      <PopUp v-if="buttontriggle" :closePopUp="closePopUp" :buttontriggle="buttontriggle">
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ex illum
+          sed saepe perferendis nulla soluta architecto deleniti, quae alias
+          error at repellendus molestias dicta ipsam aliquam. Laboriosam, vel
+          quaerat.
+        </h3>
+        <h2>My PopUp</h2>
+      </PopUp>
+			<PopUp1 v-if="timedTriggle" :closePopUp="closePopUp" :buttontriggle="buttontriggle">
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ex illum
+          sed saepe perferendis nulla soluta architecto deleniti, quae alias
+          error at repellendus molestias dicta ipsam aliquam. Laboriosam, vel
+          quaerat.
+        </h3>
+        <h2>My PopUp</h2>
+      </PopUp1>
+    </main>
+  </div>
+</template>
+
+<script>
+import PopUp from "./components/PopUp.vue";
+import PopUp1 from './components/PopUp1.vue'
+export default {
+  name: "App",
+  components: { PopUp ,PopUp1},
+  data() {
+    return {
+      buttontriggle: false,
+      timedTriggle: false,
+    };
+  },
+	methods:{
+		openPopUp(){
+			this.buttontriggle=true	
+		},
+		tttttt(){
+			setTimeout(() =>{
+        this.timedTriggle=true;
+			},2000)
+		},
+		closePopUp(){
+			this.buttontriggle=false;
+			this.timedTriggle=false
+		}
+	}
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PopUp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="popup">
+    <div class="inner-popup">
+      <slot />
+      <button @click="closePopUp">close PopUp</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    closePopUp: {
+      type: Function,
+    },
+
+  },
+};
+</script>
+<style scoped>
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.inner-popup {
+  background: #fff;
+  padding: 32px;
+  width: 200px;
+  height: fit-content;
+  box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+  border-radius: 3px solid black;
+
+}
+</style>
